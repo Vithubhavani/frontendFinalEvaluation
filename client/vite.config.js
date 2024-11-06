e5ca4bd546adce6b2b9c6034092aa7885b8ea2cf
Vithubhavani <146507010+Vithubhavani@users.noreply.github.com>
2024-11-06 17:47:10 +0530

    changed uri

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://backend-project-3wgp.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
